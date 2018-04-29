1b5ead67d978e0c086b27980c9bc9c76e648c409
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-04-29 21:52:20 +0200

    added material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
 import MasonryInfiniteScroller from 'react-masonry-infinite'
-import logo from './logo.svg'
 import './App.css'
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import { createMuiTheme } from 'material-ui/styles';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ae52d4',
+      main: '#ffffff',
+      dark: '#4a0072',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#ff79b0',
+      main: '#ff4081',
+      dark: '#c60055',
+      contrastText: '#000',
+    },
+  },
+});
 
 class App extends Component {
 
@@ -43,22 +66,36 @@ class App extends Component {
   render() {
     if ((this.state.elements.length > 0) || (this.state.firstID.length > 0)) {
       return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <MasonryInfiniteScroller
-            hasMore={true}
-            loadMore={()=>this.loadelements()}
-          >
-          {
-            this.state.elements.map((name, index) =>
-              <img src={name.src} alt = '' key = {index} width = {375} height = {(375*name.height/name.width)}/>
-            )
-          }
-          </MasonryInfiniteScroller>
-        </div>
+        <MuiThemeProvider theme={theme}>
+          <div className="App">
+            <div>
+              <AppBar position="static">
+                <Toolbar>
+                  <Typography variant="title" color="inherit">
+                    NNLO-law-Plots
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+            </div>
+            <div style = {{display: 'flex', justifyContent: 'center', padding: '10px'}}>
+            <MasonryInfiniteScroller
+              hasMore={true}
+              loadMore={()=>this.loadelements()}
+              sizes={[{ columns: 1, gutter: 10 }, { mq: '768px', columns: 2, gutter: 10 }, { mq: '1024px', columns: 3, gutter: 10 }]}
+            >
+            {
+              this.state.elements.map((name, index) =>
+              <div className = 'test'>
+                <Card>
+                  <img src={name.src} alt = '' key = {index} style={{width: '100%'}}/>
+                </Card>
+              </div>
+              )
+            }
+            </MasonryInfiniteScroller>
+            </div>
+          </div>
+        </MuiThemeProvider>
       )
     } else {
       return (
