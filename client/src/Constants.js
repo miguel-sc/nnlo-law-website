421afe0308f4335989026ddfb41ec891c37ba42f
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-05-01 16:53:14 +0200

    added Constants.js

---
+++ client/src/Constants.js
@@ -0,0 +1,14 @@
+export const filterList = [
+  {
+    name: 'PlotType',
+    subIndex: 5
+  },
+  {
+    name: 'Channel',
+    subIndex: 6
+  }
+  {
+    name: 'Observable',
+    subIndex: 7
+  }
+]
